464acf1cf5c3b2dfa4b8112cc68a2d8fb70d8db9
MrMolina10 <roberto.molina126@gmail.com>
2024-05-11 02:58:50 -0400

    Added layout components for Home page (Next step is adding scroll and organize layout)

--- new-e-portfolio/src/components/home/Home.jsx
+++ new-e-portfolio/src/components/home/Home.jsx
@@ -1,10 +1,23 @@
+import React from "react"
+import Social from "./Social"
 import "./home.css"
 
 
 const Home = () => {
     return(
         <>
-            
+            <section className="home__container" id="home">
+                <div className="home__intro">
+                    <img src="" alt="" className="home__img" />
+                    <div className="home__name">
+                        <h1>Roberto Molina</h1>
+                    </div>
+                    <span className="home__job-title">I'm a Junior Frontend Developer</span>
+                    <Social />
+                    <a href="#contact" className="btn">Hire Me</a>
+                </div>
+            </section>
+
         </>
     )
 }
---
+++ new-e-portfolio/src/components/home/Social.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+
+const Social = () => {
+    return(
+        <>
+            <div className="home__social">
+                <a href="" className="home__social-links">
+                    <i className="fa-brands fa-instagram"></i>
+                </a>
+
+                <a href="" className="home__social-links">
+                    <i className="fa-brands fa-github"></i>
+                </a>
+
+                <a href="" className="home__social-links">
+                    <i className="fa-brands fa-linkedin"></i>
+                </a>
+
+                <a href="" className="home__social-links">
+                    <i className="fa-brands fa-facebook"></i>
+                </a>
+            </div>
+        </>
+    )
+}
+
+export default Social
