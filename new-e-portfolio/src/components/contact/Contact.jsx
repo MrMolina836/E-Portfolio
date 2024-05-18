3dd8853ec8a0b24a1f183cc5deb069526b77bd4e
MrMolina10 <roberto.molina126@gmail.com>
2024-05-18 02:49:25 -0400

    Finished rough draft of contact section with proper layout (Desktop. Will be switching to laptop frames).

--- new-e-portfolio/src/components/contact/Contact.jsx
+++ new-e-portfolio/src/components/contact/Contact.jsx
@@ -1,12 +1,63 @@
 import React from "react";
 import "./contact.css";
 
-const Contact = () =>{
-    return(
-        <div className="contact__container" id="contact">
-
+const Contact = () => {
+  return (
+    <div className="contact__container" id="contact">
+      <div className="contact__layout">
+        <div className="contact__title">
+          <h1>Get in touch</h1>
+          <p>
+            I’m excited to connect with you! If you have any questions,
+            opportunities, or just want to chat, please reach out through the
+            contact section. I look forward to hearing from you and discussing
+            how we can collaborate.
+          </p>
         </div>
-    )
-}
 
-export default Contact
+        <div className="contact__form-container">
+          <form action="" className="contact__form">
+            <div className="contact__form-group">
+              <div className="contact__form-div">
+                <input
+                  type="text"
+                  className="contact__form-input"
+                  placeholder="Insert your name"
+                />
+              </div>
+
+              <div className="contact__form-div">
+                <input
+                  type="email"
+                  className="contact__form-input"
+                  placeholder="Insert your email"
+                />
+              </div>
+            </div>
+            <div className="contact__form-div">
+              <input
+                type="text"
+                className="contact__form-input"
+                placeholder="Insert your subject"
+              />
+            </div>
+
+            <div className="contact__form-div contact__form-area">
+              <textarea
+                name=""
+                className="contact__form-input"
+                cols="30"
+                rows={10}
+                placeholder="Write your message here"
+              ></textarea>
+            </div>
+
+            <button className="contact__btn">Submit </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
