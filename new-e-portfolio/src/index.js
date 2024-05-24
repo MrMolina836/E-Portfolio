5311dc400b558050efe62c22690dd4704dc6d642
MrMolina10 <roberto.molina126@gmail.com>
2024-05-24 02:51:08 -0400

    changed basename

--- new-e-portfolio/src/index.js
+++ new-e-portfolio/src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter, Routes,  Route } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <React.StrictMode>
-        <BrowserRouter basename="/repo-name">
+        <BrowserRouter basename="/E-Portfolio">
                 <Routes>
                         <Route path="*" element={<App />}> </Route>
                 </Routes>
